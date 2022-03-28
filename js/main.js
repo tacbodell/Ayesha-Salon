55c489a287f066c544b237fd1d8eb1a002f7623d
Bryson Coons <coonsbrysona@gmail.com>
2022-03-28 16:11:10 -0400

    aesthetic tweaks

--- js/main.js
+++ js/main.js
@@ -1,28 +1,17 @@
 /*FUNCTIONALITY FOR HERO SECTION BACKGROUND CAROUSEL*/ 
-
-document.querySelector('#mainArrowRight').addEventListener('click', carouselRight)
-document.querySelector('#mainArrowLeft').addEventListener('click', carouselLeft)
+document.querySelector('#mainArrowRight').addEventListener('click', carouselRight);
+document.querySelector('#mainArrowLeft').addEventListener('click', carouselLeft);
 
 const heroCarousel = document.querySelector('main');
 const heroCarouselImages = ['url(images/heroCarousel/main-bg1.jpg)','url(images/heroCarousel/main-bg2.jpg)','url(images/heroCarousel/main-bg3.jpg)'];
-let heroCarouselCurrentIndex = 0
+let heroCarouselCurrentIndex = 0;
 
 function carouselRight(){
-    if (heroCarouselCurrentIndex < 2){
-        heroCarouselCurrentIndex++;
-    } else {
-        heroCarouselCurrentIndex = 0
-    }
+    heroCarouselCurrentIndex < 2 ? heroCarouselCurrentIndex++ : heroCarouselCurrentIndex = 0;
     heroCarousel.style.backgroundImage = heroCarouselImages[heroCarouselCurrentIndex];
-    console.log(heroCarousel.style.backgroundImage)
 }
 
 function carouselLeft(){
-    if (heroCarouselCurrentIndex > 0){
-        heroCarouselCurrentIndex--;
-    } else {
-        heroCarouselCurrentIndex = 2
-    }
+    heroCarouselCurrentIndex > 0 ? heroCarouselCurrentIndex-- : heroCarouselCurrentIndex = 2;
     heroCarousel.style.backgroundImage = heroCarouselImages[heroCarouselCurrentIndex];
-    console.log(heroCarousel.style.backgroundImage)
 }
